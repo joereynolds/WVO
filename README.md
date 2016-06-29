955ffb2ddb0c1e9236c5475fff2be8c99821e0aa
Joe Reynolds <joereynolds952@gmail.com>
2016-06-29 19:12:55 +0100

    update readme. remove __repr__ add more measurements

--- README.md
+++ README.md
@@ -1,21 +1,69 @@
 # WVO
 
-A weight value object for Python with a focus on removing side-effects and mutation.
+A Weight Value Object for Python with a focus on removing side-effects and mutation.
 
 # Examples
 
+First we need to import the module, then we can begin
+`>>> import weight as w`
+
+## Addition And Subtraction
+
 ```
->>> import weight as w
->>>
 >>> x = w.Weight(5, 'lb')
 >>> y = w.Weight(56, 'kg')
 >>>
 >>> x + y
->>> 128.45886682353142lb
->>>
->>> y + x
->>> 58.26796185kg
->>>
->>> x.convert('kg')
->>> 2.2679618500000003kg
+>>> weight.Weight object at 0x7f1519a8b358>
 ```
+Every method in `Weight` brings back a new `Weight` object to reduce stateful problems.
+To get a meaningful value...
+
+```
+>>> x = w.Weight(5, 'lb')
+>>> y = w.Weight(56, 'kg')
+>>>
+>>> (x + y).value
+>>> 128.45886682353142
+>>>
+>>> (y + x).value
+>>> 58.26796185
+```
+
+Note above that the order of addition affects the way values are converted.
+If you first add `'lb' + 'kg'` as we did in the first example, then the
+resulting value will be in pounds(lb). However, if we reverse that,
+and first add the 'kg', the value will be in kilograms(kg).
+
+Subtraction behaves exactly the same
+
+```
+>>> x = w.Weight(5, 'lb')
+>>> y = w.Weight(3, 'lb')
+>>>
+>>> (x - y).value
+>>> 2
+```
+
+## Conversion
+
+```
+>>> x = w.Weight(5, 'lb')
+>>> x.convert('kg').value
+>>> 2.2679618500000003
+```
+
+# Installation and Tests
+
+## Install
+
+```
+git clone https://github.com/joereynolds/wvo
+```
+
+## Test
+
+To run the tests just do
+`python3 test.py`
+All tests should pass.
+
--- weight.py
+++ weight.py
@@ -1,40 +1,49 @@
 class Weight():
 
-
     def __init__(self, value, _type):
         self.value = value
         self.type = _type
 
     def convert(self, measurement):
+        """
+        Converts one unit of measurement to another, i.e.
+        Weight(5, 'lb').convert('kg') -> Weight(2.2679618500000003, 'kg')
+        """
         conversion_table = {
             'lb': {
                 'kg': self.value * 0.45359237,
-                'lb': self.value
+                'lb': self.value,
+                'oz': self.value * 16
+            },
+            'g' : {
+                'kg': self.value / 1000,
+                'lb': self.value * 0.0022046
             },
             'kg': {
                 'lb': self.value / 0.45359237,
-                'kg': self.value
+                'kg': self.value,
+                'g': self.value / 0.0010000
+            },
+            'oz': {
+                'oz': self.value
             }
         }
 
         return Weight(conversion_table[self.type][measurement], measurement)
 
     def __add__(self, other):
-        """Adds two weights together. Note that the unit of measurement
-        is not important, i.e. you can add 'lb' and 'kg' together.
-        When you add a 'lb' and 'kg together, it will convert
-        the 'kg' to 'lb' and add them together"""
+        """
+        Weight(5, 'lb') + Weight(6, 'kg') -> Weight(18.227735731092654, 'lb')
+        Weight(6, 'kg') + Weight(5, 'lb') -> Weight(8.26796185, 'kg')
+        """
         return Weight(
             other.convert(self.type).value + self.value,
             self.type
         )
 
     def __sub__(self, other):
-        """Same as __add__ but subtracts"""
+        """Same as addition."""
         return Weight(
             self.value - other.convert(self.type).value,
             self.type
         )
-
-    def __repr__(self):
-        return str(self.value) + self.type
